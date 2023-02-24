085f5555749618ca67f4aea3db4c6cba42d58760
Dell <hemalthakkar28@gmail.com>
2023-02-25 02:15:47 +0530

    new

--- src/Course.java
+++ src/Course.java
@@ -1,9 +1,41 @@
 public class Course extends Main {
-    int teacherid;
+    String teachername;
     String courseid;
     String coursename;
     String coursecredit;
     String coursepassingcriteria;
     int marks;
+
+    void setCourseId(String courseid)
+    {
+        this.courseid=courseid;
+    }
+
+    void setCourseName(String coursename)
+    {
+        this.coursename=coursename;
+    }
+
+    void setCourseCredit(String coursecredit)
+    {
+        this.coursecredit=coursecredit;
+    }
+
+    void setCousePassingcriteria(String coursecriteria)
+    {
+        this.coursecredit=coursecriteria;
+    }
+
+    String checkByTeacherName()
+    {
+        return this.teachername;
+    }
+
+    String checkByCourseId()
+    {
+        return this.courseid;
+    }
+    
+
 }
 
