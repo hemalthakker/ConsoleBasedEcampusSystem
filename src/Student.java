e98b152c025354eb961b746160553cadef2ee1ef
MEET <meet64946@gmail.com>
2023-02-24 20:37:09 +0530

    7

--- src/Student.java
+++ src/Student.java
@@ -13,28 +13,16 @@ public class Student {
 
         System.out.print("\nEnter Password : ");
         studentpassword = sc.nextLine();
-<<<<<<< HEAD
-=======
 
 }
+   
     void studentSignin(Scanner sc) {
         String studentusername, studentpassword;
 
-<<<<<<< HEAD
-=======
-}
-    void studentSignin(Scanner sc) {
-        String studentusername, studentpassword;
-
->>>>>>> 48b342bd4443d9f984220eee4c4f0dec5885b56d
         System.out.print("\nEnter Student UserName : ");
         studentusername = sc.nextLine();
 
         System.out.print("\nEnter Password : ");
         studentpassword = sc.nextLine();
-<<<<<<< HEAD
-=======
->>>>>>> d1095f6f63d08a83808e73f33c9631751b99d124
->>>>>>> 48b342bd4443d9f984220eee4c4f0dec5885b56d
     }
 }
