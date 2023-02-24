8a21094b0bc75d03488fd8c0b7b9894a89a165cc
MEET <meet64946@gmail.com>
2023-02-24 19:25:58 +0530

    second

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class Student {
+    void studentregistration(Scanner sc) {
+
+        String studentfullName;
+        String studentuserName, studentpassword;
+
+        System.out.print("\nEnter Full Name of Student : ");
+        studentfullName = sc.nextLine();
+
+
+        System.out.print("\nEnter UserName : ");
+        studentuserName = sc.nextLine();
+
+        System.out.print("\nEnter Password : ");
+        studentpassword = sc.nextLine();
+
+
+}
 }
--- src/Teacher.java
+++ src/Teacher.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class Teacher {
+    void teacherregistration(Scanner sc) {
+
+        String teacherfullName;
+        String teacheruserName, teacherpassword;
+
+        System.out.print("\nEnter Full Name of Teacher : ");
+        teacherfullName = sc.nextLine();
+
+
+        System.out.print("\nEnter UserName : ");
+        teacheruserName = sc.nextLine();
+
+        System.out.print("\nEnter Password : ");
+        teacherpassword = sc.nextLine();
+
+
+    }
+
 }
