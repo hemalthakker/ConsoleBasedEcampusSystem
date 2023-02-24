5e98e5c3d72c9715fc1bc220eab745efbc7d74ec
MEET <meet64946@gmail.com>
2023-02-24 21:36:16 +0530

    data3

--- src/Teacher.java
+++ src/Teacher.java
@@ -5,6 +5,20 @@ public class Teacher {
     int teacherid;
     ArrayList<Course> teachercourse = new ArrayList<Course>();
 
+    void teacher(Scanner sc)
+    {
+        System.out.print("Enter course id ");
+        String courseid = sc.nextLine();
+        System.out.print("Enter course name ");
+        String coursename = sc.nextLine();
+        System.out.print("Enter teacher id ");
+        int teacherid = Integer.parseInt(sc.nextLine());
+        System.out.print("How many student you teach ");
+        int allstudents = Integer.parseInt(sc.nextLine());
+        System.out.print("Enter student id ");
+        int studentid =  Integer.parseInt(sc.nextLine());
+
+    }
 
     void teacherRegistration(Scanner sc) {
 
